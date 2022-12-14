2b7c13c968cdc2e38d16c4014e8928788efe9ec1
unknown <thotakuraanusha998@gmail.com>
2022-12-15 00:00:59 +0530

    3dArray program corrected for different dimensions

--- SpringbootApplication/src/main/java/com/example/demo/ThreeDimensionArray.java
+++ SpringbootApplication/src/main/java/com/example/demo/ThreeDimensionArray.java
@@ -10,76 +10,39 @@ import java.util.Scanner;
  */
 
 public class ThreeDimensionArray {
-	int height;
-	int lenght;
-	int width;
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
-	public int getLenght() {
-		return lenght;
-	}
-
-	public void setLenght(int lenght) {
-		this.lenght = lenght;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
-	}
-
-	
 
 	public void printArray(int[][][] intArray) {
 
 		System.out.println("3-d array is given below :");
 		// print the elements of array
-	//	int rowNumber = intArray.length;
-	//	int coloumnNumber = intArray ;
-	//	int height = intArray.length;
-	//	intArray
-		
-		System.out.println("row number " + this.lenght);
-		System.out.println("column number " + this.width);
-		System.out.println("height " + this.height);
+		System.out.println("row number " + intArray.length);
+		System.out.println("column number " + intArray[0].length);
+		System.out.println("height " + intArray[0][0].length);
 
-		for (int i = 0; i < this.lenght; i++)
-			for (int j = 0; j < this.width; j++)
-				for (int z = 0; z < this.height; z++)   
+		for (int i = 0; i < intArray.length; i++)
+			for (int j = 0; j < intArray[0].length; j++)
+				for (int z = 0; z < intArray[0][0].length; z++)
 					System.out.println("intArray [" + i + "][" + j + "][" + z + "] = " + intArray[i][j][z]);
 	}
 
 	// here we need to write a code from reading from key board and once done it
 	// will return the 3d array;
 	public int[][][] readArray() {
-		
-		HashMap map = new HashMap();
+
 		int m, n, o, i, j, k;
 		Scanner sc = new Scanner(System.in);
 		// taking row as input
 		System.out.println("Enter the number of rows: ");
-		
+
 		// taking column as input
-		this.lenght= m = sc.nextInt();
-		
+		m = sc.nextInt();
 		System.out.println("Enter the number of columns: ");
 		// taking row as input
 		n = sc.nextInt();
-		this.width= n;
 		System.out.println("Enter the number of heigh: ");
 		o = sc.nextInt();
-		this.height =o;
-		
-		System.out.println(m+" " + n +"  " +o);
+
+		System.out.println(m + " " + n + "  " + o);
 		int[][][] intArray = new int[m][n][o];
 		System.out.println("Enter the elements of the Array: ");
 		// loop for row
@@ -97,6 +60,8 @@ public class ThreeDimensionArray {
 
 		ThreeDimensionArray td = new ThreeDimensionArray();
 		int[][][] readArray = td.readArray();
+		
+		System.out.println("============printarray starts====================");
 		td.printArray(readArray);
 
 	}
