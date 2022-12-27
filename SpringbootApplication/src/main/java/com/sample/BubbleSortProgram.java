ef5d97dd57a2abbd48bd19eba00464e0d88d1812
unknown <thotakuraanusha998@gmail.com>
2022-12-27 18:16:25 +0530

    Move bubble sort program

--- SpringbootApplication/src/main/java/com/sample/BubbleSortProgram.java
+++ SpringbootApplication/src/main/java/com/sample/BubbleSortProgram.java
@@ -1,11 +1,11 @@
-package com.example.demo1.dao;
+package com.sample;
 
 public class BubbleSortProgram {
 
 	void bubbleSort(int arr[]) {
 		int n = arr.length;
 		//System.out.println(n);
-		String str ="Anusha Java Developer";
+		String str ="Anusha Java Developer  ";
 		System.out.println("my name length is:"+ str.length());
 		for (int i = 0; i < n - 1; i++)
 			for (int j = 0; j < n - i - 1; j++)
